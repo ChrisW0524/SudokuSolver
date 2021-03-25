50331bf112a5ad85d5c3ac613a47cf346f1742f8
ChrisW0524 <chriswang20050524@gmail.com>
2021-03-24 22:37:58 -0500

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pygame
 import sys
 
-
+# Initialize pygame variables -----------------------------------------------------------------------------------------#
+pygame.init()
+clock = pygame.time.Clock()
+WINDOW_SIZE = [800, 1000]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Sudoku solver")
