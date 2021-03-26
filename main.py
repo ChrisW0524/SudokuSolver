7bce5ea648c2002f162341b445d2d32091eea39c
ChrisW0524 <chriswang20050524@gmail.com>
2021-03-26 09:22:33 -0500

    Modified grid

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 # Initialize pygame variables -----------------------------------------------------------------------------------------#
 pygame.init()
-WINDOW_SIZE = [630, 900]
+WINDOW_SIZE = [730, 900]
 FPS = 60
 ROWS = 9
 ROW_SIZE = 5
@@ -18,15 +18,19 @@ def draw(screen, size):
 
 
 def draw_grid(screen, size, ROWS):
-    gap = size[0] // ROWS
+    windowGap = 50
+    gap = (size[0] - windowGap * 2) // ROWS
+    print(gap)
     for i in range(ROWS + 1):
-        pygame.draw.line(screen, (128, 128, 128), (0, i * gap), (size[0], i * gap))
-    for i in range(ROWS):
-        pygame.draw.line(screen, (128, 128, 128), (i * gap, 0), (i * gap, size[0]))
-    for i in range(ROWS // 3 - 1):
-        pygame.draw.line(screen, (128, 128, 128), (0, (i + 1) * gap * 3), (size[0], (i + 1) * gap * 3), ROW_SIZE)
-    for i in range(ROWS // 3 - 1):
-        pygame.draw.line(screen, (128, 128, 128), ((i + 1) * gap * 3, 0), ((i + 1) * gap * 3, size[0]), ROW_SIZE)
+        pygame.draw.line(screen, (128, 128, 128), (windowGap, i * gap + windowGap),
+                         (size[0] - windowGap, i * gap + windowGap), 2)
+        pygame.draw.line(screen, (128, 128, 128), (i * gap + windowGap, windowGap),
+                         (i * gap + windowGap, size[0] - windowGap), 2)
+        if i % 3 == 0:
+            pygame.draw.line(screen, (128, 128, 128), (windowGap, i * gap + windowGap),
+                             (size[0] - windowGap, i * gap + windowGap), 4)
+            pygame.draw.line(screen, (128, 128, 128), (i * gap + windowGap, windowGap),
+                             (i * gap + windowGap, size[0] - windowGap), 4)
 
 
 # Main game loop ------------------------------------------------------------------------------------------------------#
