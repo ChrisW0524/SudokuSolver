254010fcd86b4e265b57290df9810a5c9d3180d1
ChrisW0524 <chriswang20050524@gmail.com>
2021-04-03 23:28:45 -0500

    Added selection function

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ WINDOW_SIZE = [730, 900]
 FPS = 60
 ROWS = 9
 ROW_SIZE = 5
+WINDOW_GAP = 50
+WINDOW_LENGTH = WINDOW_SIZE[0] - 2 * WINDOW_GAP
 mainClock = pygame.time.Clock()
 mainScreen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Sudoku solver")
 
-
+# Draw elements
 def draw(screen, size, grid):
     screen.fill((255, 255, 255))
     for i in grid:
@@ -21,40 +23,68 @@ def draw(screen, size, grid):
             j.draw(screen)
     draw_grid(screen, size, ROWS)
 
-
+# Draw grid
 def draw_grid(screen, size, ROWS):
-    windowGap = 50
-    gap = (size[0] - windowGap * 2) // ROWS
+    gap = (size[0] - WINDOW_GAP * 2) // ROWS
     for i in range(ROWS + 1):
-        pygame.draw.line(screen, (128, 128, 128), (windowGap, i * gap + windowGap),
-                         (size[0] - windowGap, i * gap + windowGap), 2)
-        pygame.draw.line(screen, (128, 128, 128), (i * gap + windowGap, windowGap),
-                         (i * gap + windowGap, size[0] - windowGap), 2)
+        pygame.draw.line(screen, nodeClass.BLACK, (WINDOW_GAP, i * gap + WINDOW_GAP),
+                         (size[0] - WINDOW_GAP, i * gap + WINDOW_GAP), 2)
+        pygame.draw.line(screen, nodeClass.BLACK, (i * gap + WINDOW_GAP, WINDOW_GAP),
+                         (i * gap + WINDOW_GAP, size[0] - WINDOW_GAP), 2)
         if i % 3 == 0:
-            pygame.draw.line(screen, (128, 128, 128), (windowGap, i * gap + windowGap),
-                             (size[0] - windowGap, i * gap + windowGap), 4)
-            pygame.draw.line(screen, (128, 128, 128), (i * gap + windowGap, windowGap),
-                             (i * gap + windowGap, size[0] - windowGap), 4)
+            pygame.draw.line(screen, nodeClass.BLACK, (WINDOW_GAP, i * gap + WINDOW_GAP),
+                             (size[0] - WINDOW_GAP, i * gap + WINDOW_GAP), 4)
+            pygame.draw.line(screen, nodeClass.BLACK, (i * gap + WINDOW_GAP, WINDOW_GAP),
+                             (i * gap + WINDOW_GAP, size[0] - WINDOW_GAP), 4)
 
+# Initialize grid with nodes
 def make_grid(size):
     grid = []
-    windowGap = 50
-    gap = (size[0] - windowGap * 2) // ROWS
+    gap = (size[0] - WINDOW_GAP * 2) // ROWS
     for i in range(9):
         grid.append([])
         for j in range(9):
-            node = nodeClass.Node(i, j, gap, random.randint(1,9), 50)
+            node = nodeClass.Node(i, j, gap, random.randint(1, 9), 50)
             grid[i].append(node)
     return grid
+
+# Translates mouse pos to grid pos
+def get_clicked_pos(size):
+    x, y = pygame.mouse.get_pos()
+    gap = (size[0] - WINDOW_GAP * 2) // ROWS
+    row = (x - WINDOW_GAP) // gap
+    col = (y - WINDOW_GAP) // gap
+    return row, col
 
 
 # Main game loop ------------------------------------------------------------------------------------------------------#
 def main(screen, size, clock):
+    # Initialize game loop variables
     run = True
     grid = make_grid(size)
+
+    selected = None
+
     while run:
+        # Draws elements
         draw(screen, size, grid)
+
+        # Event loop
         for event in pygame.event.get():
+            # Mouse input (left click)
+            if pygame.mouse.get_pressed()[0]:
+                # Make selection node
+                if WINDOW_GAP <= pygame.mouse.get_pos()[0] <= WINDOW_LENGTH + WINDOW_GAP and WINDOW_GAP <= pygame.mouse.get_pos()[1] <= WINDOW_LENGTH + WINDOW_GAP:
+                    row, col = get_clicked_pos(size)
+                    spot = grid[row][col]
+                    # Reset other nodes
+                    if selected:
+                        for i in grid:
+                            for j in i:
+                                j.reset()
+                    selected = spot
+                    spot.make_selected()
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
--- nodeClass.py
+++ nodeClass.py
@@ -1,5 +1,16 @@
 import pygame
 
+# Initialize colors (colors control state of cell) --------------------------------------------------------------------#
+RED = (255, 0, 0) # Error
+GREEN = (0, 255, 0) # Solved
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255) # Default
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128) # Selected
+TURQUOISE = (64, 244, 208)
 
 class Node:
     def __init__(self, row, col, size, value, offset):
@@ -23,5 +34,17 @@ class Node:
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
         font = pygame.font.SysFont(None, 60)
-        screen_text = font.render(str(self.value), True, (128, 128, 128))
+        screen_text = font.render(str(self.value), True, BLACK)
         screen.blit(screen_text, (self.x + self.size / 2 - screen_text.get_width()/2, self.y + self.size / 2  - screen_text.get_height()/2))
+
+    def reset(self):
+        self.color = WHITE
+
+    def make_selected(self):
+        self.color = GREY
+
+    def make_solved(self):
+        self.color = GREEN
+
+    def make_error(self):
+        self.color = RED
